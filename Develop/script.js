22c4edf62a090ab9c0aec2dc38d616d473066c9c
Julez Le <simplylosectrl@gmail.com>
2020-05-03 16:25:04 -0600

    Color coding works

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,22 @@
-
+$(document).ready(function () {
 //1. get date and append to html
 var date=moment().format('dddd, MMMM Do YYYY');
 //2. get currenthour (so we can later add present future or past)
-var currentHour=moment().format('HH:MM');    //9
+var currentHour=parseInt(moment().format('HH'));    //9
 
 $("#currentDay").append(date);
 $("#currentTime").append(currentHour);
 
+//input text variable
+var saveEvent;
+//time of event variable
+var saveHr;
+
 //3.dynamically append text area
     //a. an array of hours 
-    var timeArray=[09,10,11,12,13,14,15,16,17];
+    var timeArray=[9,10,11,12,13,14,15,16,17];
+    console.log(timeArray)
     var timeDisplay = ["9:00am","10:00am","11:00am","12:00pm","1:00pm","2:00pm","3:00pm","4:00pm","5:00pm"]
-    var reverseTime = timeArray.reverse();
     
 // <div class="input-group mb-3">
 //     <div class="input-group-prepend">
@@ -21,7 +26,6 @@ $("#currentTime").append(currentHour);
 //     <button class="btn btn-outline-secondary" type="button" id="save">Save</button>
 // </div> 
 
-$(document).ready(function () {
 //4. run a for loop to dynamically add the rest of the text area
 
     for(var j = 0; j < timeDisplay.length; j++) {
@@ -37,8 +41,25 @@ $(document).ready(function () {
         
         span.text(timeDisplay[j]);
         //<span></span>
+       
         var input=$("<input>");
-        input.attr("class", "form-control");
+
+        console.log(timeArray[j])
+        console.log("inside forloop")
+        span.attr("id", timeArray[j]);
+ 
+        if (currentHour > timeArray[j]) {
+            input.attr("class", "form-control input past");
+
+        }
+        if(currentHour==timeArray[j]){
+            input.attr("class", "form-control input present");
+        }
+        if(currentHour < timeArray[j]) {
+            input.attr("class", "form-control input future");
+        }
+        //add id so user can grab time and userinput in the same area
+        input.attr("id", timeArray[j]);
         //<input>
         var btn=$("<button>");
         btn.attr("class", "btn btn-outline-secondary");
@@ -53,16 +74,7 @@ $(document).ready(function () {
 
         $(".container").append(d1);
     
-    for(var i = 0; i < (reverseTime.length); i++) {
-        span.attr("id", reverseTime[i]++);
     }
-
-    };
-});
-//a. add styling
-//currentHour==timeArray[i] add present class
-//currentHour <timeArray[i] add future class
-//currentHour >timeArray[i] add past class
 
 
 
@@ -70,5 +82,15 @@ $(document).ready(function () {
     //a. grab user value
     //b. grab current hour
     //c. set to localstorage hour=value (9="Testing")
+
+    $("#saveBtn").click(function (event) {
+      event.preventDefault();
+      alert("clicked");
+
+    });
+
+});
+
+
 
 //6. get the data from localstorage and append it to the associated textarea (shows persistance)
